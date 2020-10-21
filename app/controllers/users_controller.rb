3876a066c81aa19a3a48fcebb5d5bfff03c94353
Ryoka Mizoue <feh14615@kwansei.ac.jp>
2020-10-22 00:15:58 +0900

    Release to heroku

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,8 @@
 class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
-    @m0 = Post.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day).where(user_id: @user).sum(:time)
+    m0 = Post.where(created_at: Time.zone.now.beginning_of_day..Time.zone.now.end_of_day).where(user_id: @user)
+    @m0 = m0.sum(:time)
     @m1 = Post.where(created_at: Time.zone.now.ago(1.days).beginning_of_day..Time.zone.now.ago(1.days).end_of_day).where(user_id: @user).sum(:time)
     @m2 = Post.where(created_at: Time.zone.now.ago(2.days).beginning_of_day..Time.zone.now.ago(2.days).end_of_day).where(user_id: @user).sum(:time)
     @m3 = Post.where(created_at: Time.zone.now.ago(3.days).beginning_of_day..Time.zone.now.ago(3.days).end_of_day).where(user_id: @user).sum(:time)
