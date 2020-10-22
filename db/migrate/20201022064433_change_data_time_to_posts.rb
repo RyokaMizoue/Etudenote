c929cdaec24aba23e4c079ee0714d200c97f60b5
Ryoka Mizoue <feh14615@kwansei.ac.jp>
2020-10-22 16:38:07 +0900

    Release to heroku

--- db/migrate/20201022064433_change_data_time_to_posts.rb
+++ db/migrate/20201022064433_change_data_time_to_posts.rb
@@ -1,5 +1,5 @@
 class ChangeDataTimeToPosts < ActiveRecord::Migration[6.0]
   def change
-    change_column :posts, :time, :integer
+    change_column :posts, :time, 'integer USING CAST(time AS integer)'
   end
 end
